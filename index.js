4784070be0aac5b9787bbad1717287afaf8c51ff
LiHui <805512979@qq.com>
2019-01-29 15:37:28 +0800

    核心功能基本完成

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 /*
 *小肥牛扫码点餐项目API子系统
 */
-
-
-
-const port = 8091;
+const PORT = 8091; 
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const categoryRouter =require('./routes/admin/category');
-const userAdmin = require('./routes/admin/admin')
+const categoryRouter = require('./routes/admin/category');
+const adminRouter = require('./routes/admin/admin');
+const dishRouter = require('./routes/admin/dish');
 
-//创建http应用服务器
-var app = express();
-app.listen(port, ()=>{ 
-  console.log('API服务器启动成功,Server Listening'+port);
-  console.log(new Date().toLocaleString());
+//创建HTTP应用服务器
+var app = express(); 
+app.listen(PORT, ()=>{
+  console.log('Server Listening: '+PORT);
 });
 
 //使用中间件
 app.use(cors());
-app.use(bodyParser.json());//吧json格式的請求主体数据解析放在req.body属性
+//app.use(bodyParser.urlencoded({}))  //把application/x-www-form-urlencoded格式的请求主体数据解析出来放入req.body属性
+app.use(bodyParser.json()); //把application/json格式的请求主体数据解析出来放入req.body属性
+
 //挂载路由器
-app.use('/admin/category',categoryRouter);
-app.use('/admin',userAdmin);
+app.use('/admin/category', categoryRouter);
+app.use('/admin', adminRouter);
+app.use('/admin/dish', dishRouter);
